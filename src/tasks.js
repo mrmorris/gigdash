5f80ef492a6e48ff795b45dbf8e3a2db3631c11c
Anathomical <anathomical@anathomical.com>
2020-07-11 14:35:30 -0400

    Add reviews to task setup

--- src/tasks.js
+++ src/tasks.js
@@ -3,8 +3,8 @@ import Task from 'entities/Task.js'
 
 
 export default [
-  new Task(1, 'Buy me an apple', 'John', C.NEIGHBORHOOD_FOX_POINT, [C.ITEM_BANANA]),
-  new Task(2, 'I am hungry', 'Jane', C.NEIGHBORHOOD_FOX_POINT, [C.ITEM_HAMMER, C.ITEM_ENERGY_DRINK]),
-  new Task(3, 'So lonely 😃', 'Smirny', C.NEIGHBORHOOD_OLNEYVILLE, [C.ITEM_VODKA]);
-  new Task(4, 'Buy me another apple', 'Cricket', C.NEIGHBORHOOD_FEDERAL_HALL, [C.ITEM_BANANA])
+  new Task(1, 'Buy me an apple', 'John', C.NEIGHBORHOOD_FOX_POINT, [C.ITEM_BANANA], 'Love it', 'Hate it'),
+  new Task(2, 'I am hungry', 'Jane', C.NEIGHBORHOOD_FOX_POINT, [C.ITEM_HAMMER, C.ITEM_ENERGY_DRINK], 'Thanks!', 'You suck!'),
+  new Task(3, 'So lonely 😃', 'Smirny', C.NEIGHBORHOOD_OLNEYVILLE, [C.ITEM_VODKA], 'So so...', 'Meh');
+  new Task(4, 'Buy me another apple', 'Cricket', C.NEIGHBORHOOD_FEDERAL_HALL, [C.ITEM_BANANA], 'Marry me!', 'Can you help me?')
 ]
