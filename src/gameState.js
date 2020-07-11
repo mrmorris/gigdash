53031b34898d95a0b9a42c98039a4558eba6061c
Ryan Morris <mr.morris@gmail.com>
2020-07-11 09:39:27 -0400

    Game state

---
+++ src/gameState.js
@@ -0,0 +1,18 @@
+const tasks = [];
+const inventory = [];
+
+export const addTask = () => {
+
+};
+
+export const getTasks = () => {
+
+};
+
+export const completeTask = () => {
+
+};
+
+export const getInventory = () => {
+
+};
