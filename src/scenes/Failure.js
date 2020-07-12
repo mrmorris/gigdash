3275f36a34564c645fe3a433ac898ab3844b1332
Thomas Robertson <thomas.robertson@quill.org>
2020-07-12 14:04:21 -0400

    Add and format final failure text on the failure screen

--- src/scenes/Failure.js
+++ src/scenes/Failure.js
@@ -8,6 +8,17 @@ export default class extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(100, 100, 'Failure');
+    const failureText = `
+Dear Miles,
+
+Due to your absolutely abysmal delivery approval rating through our ExtraMiles procurement and courier service, we can no longer offer you gainful employment at this time. Please return your vehicle to the nearest ExtraMiles depot as soon as humanly possible before we seek legal action or more violent punitive measures. An invoice will be sent seprately to bill you for the remainder of your vehicle usage fees and ExtraMiles memberships dues.
+
+Cheers!
+ExtraMiles
+`
+
+    this.add.text(100, 100, failureText, {
+      wordWrap: { width: 400 }
+    });
   }
 }
